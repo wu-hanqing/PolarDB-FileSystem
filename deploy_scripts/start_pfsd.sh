38e7c3d4f9258c5f89e6a2e1e92c5dfc7858df5f
yfxu <yfxu@corp.netease.com>
2022-04-13 10:46:14 +0800

    redirect stderr correctly.

--- deploy_scripts/start_pfsd.sh
+++ deploy_scripts/start_pfsd.sh
@@ -35,7 +35,7 @@ fi
 
 ulimit -c unlimited
 
-nohup ${BASE_DIR}/../bin/pfsdaemon $* -c ${CONF_FILE}/pfsd_logger.conf -f 2>&1 1>/dev/null &
+nohup ${BASE_DIR}/../bin/pfsdaemon $* -c ${CONF_FILE}/pfsd_logger.conf -f 1>/dev/null 2>&1 &
 
 sleep 1
 
