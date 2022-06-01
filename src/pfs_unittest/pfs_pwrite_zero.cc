c40104c30f6864bcb94b49e2e4fe6c771d5c3283
Xu Yifeng <yfxu@donnot_mail_tome.com>
2022-06-01 17:05:16 +0800

    add pwrite perf test.

---
+++ src/pfs_unittest/pfs_pwrite_zero.cc
@@ -0,0 +1,121 @@
+#include <err.h>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <chrono>
+#include <rte_malloc.h>
+#include <rte_cycles.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include "pfs_api.h"
+
+using namespace std;
+
+DEFINE_string(cluster, "", "cluster name");
+DEFINE_int32(host_id, 1, "hosit id");
+DEFINE_string(pbd_name, "", "pbdname name");                       
+
+extern int                                                                             
+pfs_spdk_dev_io_get_cpu_stats(const char *devname,                              
+        uint64_t * busy_tsc, uint64_t *idle_tsc);
+
+int main(int argc, char **argv)
+{
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+	int hostid = FLAGS_host_id;
+	string cluster = FLAGS_cluster;
+	string pbdname = FLAGS_pbd_name;
+
+	if (cluster.empty()) {
+		std::cout << "cluster is empty";
+		return 1;
+	}
+	if (pbdname.empty()) {
+		std::cout << "pbd_name is empty";
+		return 1;
+	}
+
+	                                                                               
+	int flags = MNTFLG_TOOL | MNTFLG_RDWR | MNTFLG_LOG;
+	int rc = pfs_mount(cluster.c_str(), pbdname.c_str(), hostid, flags);
+	if (rc < 0) {                                                              
+        	cout << "pfs_mount failed." << endl;                                    
+		return 1;
+    	} 
+	cout << "Setup successful" << endl;       	
+
+	std::string path;
+	path = "/" + pbdname + "/zero_write_file";
+	int fd = pfs_open(path.c_str(), O_RDWR|O_CREAT, 0600);
+	if (fd < 0) {
+		cout << "can not open file\n";
+		return 1;
+	}
+
+#define DO(func, ...) 			\
+	{				\
+	struct timeval tv1, tv2; \
+	struct rusage ru1, ru2; \
+        getrusage(RUSAGE_THREAD, &ru1); \
+	auto start = std::chrono::steady_clock::now(); \
+	for (int j = 0; j < loops; ++j) { \
+		rc = pfs_lseek(fd, 0, SEEK_SET); \
+		if (rc == -1) { \
+			err(1, "pfs_lseek"); \
+		} \
+		\
+		for (int i = 0; i < blocks; ++i) { \
+			rc = func(__VA_ARGS__); \
+			if (rc != buf_sz) { \
+				err(1, #func); \
+			} \
+		} \
+	} \
+	auto end = std::chrono::steady_clock::now(); \
+        getrusage(RUSAGE_THREAD, &ru2); \
+	timersub(&ru2.ru_utime, &ru1.ru_utime, &tv1); \
+	timersub(&ru2.ru_stime, &ru1.ru_stime, &tv2); \
+	timeradd(&tv1, &tv2, &tv1); \
+	std::chrono::duration<double> elapsed_seconds = end-start; \
+	std::cout << #func << " elapsed time: " << elapsed_seconds.count() << "s"; \
+	std::cout << ", used cpu time:" << tv1.tv_sec + double(tv1.tv_usec)/1000000 << "s\n"; \
+	}
+
+	std::cout << "initializing...\n";
+	rc = pfs_ftruncate(fd, 0);
+	if (rc) {
+		err(1, "pfs_ftruncat");
+	}
+	
+	const int buf_sz = 16384;
+	const int blocks = 10000;
+	const int loops = 20;
+	char buf[buf_sz];
+	memset(buf, 0, sizeof(buf));
+	for (int i = 0; i < blocks; ++i) {
+		rc = pfs_write(fd, buf, buf_sz);
+		if (rc != sizeof(buf)) {
+			err(1, "pfs_write");
+		}
+	}
+	std::cout << "initialized.\n";
+
+	char *dma_buf = (char *)rte_malloc("", buf_sz, 64);
+	memset(dma_buf, 0, sizeof(buf_sz));
+
+	DO(pfs_write, fd, buf, buf_sz);
+	DO(pfs_write_dma, fd, dma_buf, buf_sz);
+	DO(pfs_write_zero, fd, buf_sz);
+
+	rte_free(dma_buf);
+
+	pfs_umount(pbdname.c_str());
+	return 0;
+}
