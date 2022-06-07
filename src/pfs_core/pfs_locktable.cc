c0d1859c844aeaae3ad56b3273c9bfc6d7c8f957
Xu Yifeng <yfxu@donnot_mail_tome.com>
2022-06-07 14:44:54 +0800

    update copyright.

--- src/pfs_core/pfs_locktable.cc
+++ src/pfs_core/pfs_locktable.cc
@@ -1,3 +1,26 @@
+/*
+ *  Copyright (c) 2020 NetEase Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+/**
+ * Project : curve
+ * Date : 2022/06/07
+ * Author: XuYifeng
+ */
+
 #include "pfs_locktable.h"
 #include "pfs_rangelock.h"
 #include "pfs_util.h"
@@ -7,8 +30,8 @@
 #include <rte_stack.h>
 #include <rte_malloc.h>
 
-#define LOCKTABLE_BITS 12
-#define LOCKTABLE_SIZE (1 << 12)
+#define LOCKTABLE_BITS 10
+#define LOCKTABLE_SIZE (1 << LOCKTABLE_BITS)
 
 #define LOCKTABLE_ITEM_CACHE 1024
 #define LOCKTABLE_ITEM_WARM  128
